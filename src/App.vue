c26783439918de055b0c185ab30fd4a95f25d134
ASGart <admin@MacBook-Air-admin.local>
2026-02-01 16:08:38 +0700

    added button component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script setup>
-  import { ref } from 'vue'
-
-  const date = new Date()
-  const time = ref('')
-  calcTime()
-  function calcTime() {
-    const currentDateTime = new Date()
-    let currentTime = ''
-    currentTime = currentDateTime.getHours()
-    currentTime += ':'
-    currentTime += currentDateTime.getMinutes()
-    currentTime += ':'
-    currentTime += currentDateTime.getSeconds() < 10 ? '0' + currentDateTime.getSeconds() : currentDateTime.getSeconds()
-    time.value = currentTime
-  }
-  setInterval(() => {
-    calcTime()
-  }, 1000)
+  import Button from './components/Button.vue'
 </script>
 
 <template>
-  <div>
-    <p>Текущая дата: <strong>{{  date.toLocaleDateString() }}</strong></p>
-    <p>Текущее время: <strong>{{ time }}</strong></p>
+  <div class="main">
+    <Button>Начать игру</Button>
   </div>
 </template>
 
---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+<template>
+    <button class="button"><slot></slot></button>
+</template>
+<style scoped>
+    .button {
+        width: 315px;
+        border-radius: 100px;
+        padding: 16px 10;
+        font-size: 24px;
+        line-height: 36px;
+        letter-spacing: 0%;
+        text-align: center;
+        border: none;
+        background: var(--color-primary);
+        color: var(--color-text-primary-invert);
+        transition: 0.5s;
+    }
+    .button:hover {
+        background: var(--color-primary-darken);
+    }
+</style>
